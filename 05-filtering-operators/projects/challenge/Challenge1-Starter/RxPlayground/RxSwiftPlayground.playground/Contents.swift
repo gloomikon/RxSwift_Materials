9a9e0e6aec3b4be1291cdb9a61a3d16a696da73b
Nikolay Zhurba <gloomikon@gmail.com>
2021-08-06 14:57:03 +0300

    Chapter 5: Challange

--- 05-filtering-operators/projects/challenge/Challenge1-Starter/RxPlayground/RxSwiftPlayground.playground/Contents.swift
+++ 05-filtering-operators/projects/challenge/Challenge1-Starter/RxPlayground/RxSwiftPlayground.playground/Contents.swift
@@ -2,69 +2,69 @@ import Foundation
 import RxSwift
 
 example(of: "Challenge 1") {
-  let disposeBag = DisposeBag()
-  
-  let contacts = [
-    "603-555-1212": "Florent",
-    "212-555-1212": "Shai",
-    "408-555-1212": "Marin",
-    "617-555-1212": "Scott"
-  ]
-  
-  func phoneNumber(from inputs: [Int]) -> String {
-    var phone = inputs.map(String.init).joined()
+    let disposeBag = DisposeBag()
     
-    phone.insert("-", at: phone.index(
-      phone.startIndex,
-      offsetBy: 3)
-    )
+    let contacts = [
+        "603-555-1212": "Florent",
+        "212-555-1212": "Shai",
+        "408-555-1212": "Marin",
+        "617-555-1212": "Scott"
+    ]
     
-    phone.insert("-", at: phone.index(
-      phone.startIndex,
-      offsetBy: 7)
-    )
-    
-    return phone
-  }
-  
-  let input = PublishSubject<Int>()
-  
-  input
-    .skipWhile { $0 == 0 }
-    .filter { $0 < 10 }
-    .take(10)
-    .toArray()
-    .subscribe(onSuccess: { inputs in
-        let phone = phoneNumber(from: inputs)
-        if let contact = contacts[phone] {
-            print("Dialing \(contact) (\(phone))...")
-        } else {
-            print("Contact not found")
-        }
-    },
-    onError: { error in
-        print(error)
-    })
-    .disposed(by: disposeBag)
-    
-  
-  input.onNext(0)
-  input.onNext(603)
-  
-  input.onNext(2)
-  input.onNext(1)
-  
-  // Confirm that 7 results in "Contact not found",
-  // and then change to 2 and confirm that Shai is found
-  input.onNext(2)
-  
-  "5551212".forEach {
-    if let number = (Int("\($0)")) {
-      input.onNext(number)
+    func phoneNumber(from inputs: [Int]) -> String {
+        var phone = inputs.map(String.init).joined()
+        
+        phone.insert("-", at: phone.index(
+                        phone.startIndex,
+                        offsetBy: 3)
+        )
+        
+        phone.insert("-", at: phone.index(
+                        phone.startIndex,
+                        offsetBy: 7)
+        )
+        
+        return phone
     }
-  }
-  
-  input.onNext(9)
+    
+    let input = PublishSubject<Int>()
+    
+    input
+        .skipWhile { $0 == 0 }
+        .filter { $0 < 10 }
+        .take(10)
+        .toArray()
+        .subscribe(onSuccess: { inputs in
+            let phone = phoneNumber(from: inputs)
+            if let contact = contacts[phone] {
+                print("Dialing \(contact) (\(phone))...")
+            } else {
+                print("Contact not found")
+            }
+        },
+        onError: { error in
+            print(error)
+        })
+        .disposed(by: disposeBag)
+    
+    
+    input.onNext(0)
+    input.onNext(603)
+    
+    input.onNext(2)
+    input.onNext(1)
+    
+    // Confirm that 7 results in "Contact not found",
+    // and then change to 2 and confirm that Shai is found
+    input.onNext(2)
+    
+    "5551212".forEach {
+        if let number = (Int("\($0)")) {
+            input.onNext(number)
+        }
+    }
+    
+    input.onNext(9)
 }
 
 /// Copyright (c) 2020 Razeware LLC
