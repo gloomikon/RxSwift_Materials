e6aee18a4b8f10f6ac4daff1ab5c637d8acc053c
Nikolay Zhurba <gloomikon@gmail.com>
2021-08-12 16:13:53 +0300

    Chapter 11

--- 11-time-based-operators/projects/starter/RxSwiftPlayground.playground/Pages/replay.xcplaygroundpage/Contents.swift
+++ 11-time-based-operators/projects/starter/RxSwiftPlayground.playground/Pages/replay.xcplaygroundpage/Contents.swift
@@ -2,9 +2,36 @@ import UIKit
 import RxSwift
 import RxCocoa
 
+let elementsPerSecond = 1
+let maxElements = 5
+let replayedElements = 1
+let replayDelay: TimeInterval = 3
 
-// Start coding here
+let sourceObservable = Observable<Int>
+  .interval(.milliseconds(Int(1000.0 / Double(elementsPerSecond))), scheduler: MainScheduler.instance)
+  .replay(replayedElements)
 
+let sourceTimeline = TimelineView<Int>.make()
+let replayedTimeline = TimelineView<Int>.make()
+
+let stack = UIStackView.makeVertical([
+  UILabel.makeTitle("replay"),
+  UILabel.make("Emit \(elementsPerSecond) per second:"),
+  sourceTimeline,
+  UILabel.make("Replay \(replayedElements) after \(replayDelay) sec:"),
+  replayedTimeline])
+
+_ = sourceObservable.subscribe(sourceTimeline)
+
+DispatchQueue.main.asyncAfter(deadline: .now() + replayDelay) {
+  _ = sourceObservable.subscribe(replayedTimeline)
+}
+
+_ = sourceObservable.connect()
+
+let hostView = setupHostView()
+hostView.addSubview(stack)
+hostView
 
 // Support code -- DO NOT REMOVE
 class TimelineView<E>: TimelineViewBase, ObserverType where E: CustomStringConvertible {
@@ -23,17 +50,17 @@ class TimelineView<E>: TimelineViewBase, ObserverType where E: CustomStringConve
   }
 }
 /// Copyright (c) 2020 Razeware LLC
-/// 
+///
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-/// 
+///
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-/// 
+///
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -41,7 +68,7 @@ class TimelineView<E>: TimelineViewBase, ObserverType where E: CustomStringConve
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-/// 
+///
 /// This project and source code may use libraries or frameworks that are
 /// released under various Open-Source licenses. Use of those libraries and
 /// frameworks are governed by their own individual licenses.
--- 11-time-based-operators/projects/starter/RxSwiftPlayground.playground/Pages/window.xcplaygroundpage/Contents.swift
+++ 11-time-based-operators/projects/starter/RxSwiftPlayground.playground/Pages/window.xcplaygroundpage/Contents.swift
@@ -3,7 +3,47 @@ import RxSwift
 import RxCocoa
 
 
-// Start coding here
+let elementsPerSecond = 3
+let windowTimeSpan: RxTimeInterval = .seconds(4)
+let windowMaxCount = 10
+let sourceObservable = PublishSubject<String>()
+
+let sourceTimeline = TimelineView<String>.make()
+
+let stack = UIStackView.makeVertical([
+  UILabel.makeTitle("window"),
+  UILabel.make("Emitted elements (\(elementsPerSecond) per sec.):"),
+  sourceTimeline,
+  UILabel.make("Windowed observables (at most \(windowMaxCount) every \(windowTimeSpan) sec):")])
+
+let timer = DispatchSource.timer(interval: 1.0 / Double(elementsPerSecond), queue: .main) {
+  sourceObservable.onNext("🐱")
+}
+
+_ = sourceObservable.subscribe(sourceTimeline)
+
+_ = sourceObservable
+  .window(timeSpan: windowTimeSpan, count: windowMaxCount, scheduler: MainScheduler.instance)
+  .flatMap { windowedObservable -> Observable<(TimelineView<Int>, String?)> in
+    let timeline = TimelineView<Int>.make()
+    stack.insert(timeline, at: 4)
+    stack.keep(atMost: 8)
+    return windowedObservable
+      .map { value in (timeline, value) }
+      .concat(Observable.just((timeline, nil)))
+  }
+  .subscribe(onNext: { tuple in
+    let (timeline, value) = tuple
+    if let value = value {
+      timeline.add(.next(value))
+    } else {
+      timeline.add(.completed(true))
+    }
+  })
+
+let hostView = setupHostView()
+hostView.addSubview(stack)
+hostView
 
 
 
@@ -25,35 +65,3 @@ class TimelineView<E>: TimelineViewBase, ObserverType where E: CustomStringConve
     }
   }
 }
-
-/// Copyright (c) 2020 Razeware LLC
-/// 
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-/// 
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-/// 
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-/// distribute, sublicense, create a derivative work, and/or sell copies of the
-/// Software in any work that is designed, intended, or marketed for pedagogical or
-/// instructional purposes related to programming, coding, application development,
-/// or information technology.  Permission for such use, copying, modification,
-/// merger, publication, distribution, sublicensing, creation of derivative works,
-/// or sale is expressly withheld.
-/// 
-/// This project and source code may use libraries or frameworks that are
-/// released under various Open-Source licenses. Use of those libraries and
-/// frameworks are governed by their own individual licenses.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.
