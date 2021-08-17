36e4f1f27d04b7e86313f2a474d9fbe2fb77e903
Nikolay Zhurba <gloomikon@gmail.com>
2021-08-17 15:32:27 +0300

    Chapter 14

--- 14-error-handling-in-practice/projects/starter/Wundercast/ViewController.swift
+++ 14-error-handling-in-practice/projects/starter/Wundercast/ViewController.swift
@@ -1,36 +1,3 @@
-/// Copyright (c) 2020 Razeware LLC
-/// 
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-/// 
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-/// 
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-/// distribute, sublicense, create a derivative work, and/or sell copies of the
-/// Software in any work that is designed, intended, or marketed for pedagogical or
-/// instructional purposes related to programming, coding, application development,
-/// or information technology.  Permission for such use, copying, modification,
-/// merger, publication, distribution, sublicensing, creation of derivative works,
-/// or sale is expressly withheld.
-/// 
-/// This project and source code may use libraries or frameworks that are
-/// released under various Open-Source licenses. Use of those libraries and
-/// frameworks are governed by their own individual licenses.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.
-
-import UIKit
 import RxSwift
 import RxCocoa
 import MapKit
@@ -40,133 +7,202 @@ typealias Weather = ApiController.Weather
 
 class ViewController: UIViewController {
 
-  @IBOutlet weak var keyButton: UIButton!
-  @IBOutlet weak var geoLocationButton: UIButton!
-  @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
-  @IBOutlet weak var searchCityName: UITextField!
-  @IBOutlet weak var tempLabel: UILabel!
-  @IBOutlet weak var humidityLabel: UILabel!
-  @IBOutlet weak var iconLabel: UILabel!
-  @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var keyButton: UIButton!
+    @IBOutlet weak var geoLocationButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var searchCityName: UITextField!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var iconLabel: UILabel!
+    @IBOutlet weak var cityNameLabel: UILabel!
 
-  private let bag = DisposeBag()
-  private let locationManager = CLLocationManager()
+    private let bag = DisposeBag()
+    private let locationManager = CLLocationManager()
+    private var cache = [String: Weather]()
 
-  var keyTextField: UITextField?
+    var keyTextField: UITextField?
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
-    style()
+        if RxReachability.shared.startMonitor("apple.com") == false {
+            print("Reachability failed!")
+          }
 
-    keyButton.rx.tap
-      .subscribe(onNext: { [weak self] _ in
-        self?.requestKey()
-      })
-      .disposed(by:bag)
+        style()
 
-    let currentLocation = locationManager.rx.didUpdateLocations
-      .map { locations in locations[0] }
-      .filter { location in
-        return location.horizontalAccuracy == kCLLocationAccuracyNearestTenMeters
-      }
+        keyButton.rx.tap
+            .subscribe(onNext: { [weak self] _ in
+                self?.requestKey()
+            })
+            .disposed(by:bag)
 
-    let geoInput = geoLocationButton.rx.tap
-      .do(onNext: { [weak self] _ in
-        self?.locationManager.requestWhenInUseAuthorization()
-        self?.locationManager.startUpdatingLocation()
+        let currentLocation = locationManager.rx.didUpdateLocations
+            .map { locations in locations[0] }
+            .filter { location in
+                return location.horizontalAccuracy == kCLLocationAccuracyNearestTenMeters
+            }
 
-        self?.searchCityName.text = "Current Location"
-      })
+        let geoInput = geoLocationButton.rx.tap
+            .do(onNext: { [weak self] _ in
+                self?.locationManager.requestWhenInUseAuthorization()
+                self?.locationManager.startUpdatingLocation()
 
-    let geoLocation = geoInput.flatMap {
-      return currentLocation.take(1)
+                self?.searchCityName.text = "Current Location"
+            })
+
+        let geoLocation = geoInput.flatMap {
+            return currentLocation.take(1)
+        }
+
+        let geoSearch = geoLocation.flatMap { location in
+            return ApiController.shared.currentWeather(at: location.coordinate)
+                .catchErrorJustReturn(.empty)
+        }
+
+        let maxAttempts = 4
+
+        let retryHandler: (Observable<Error>) -> Observable<Int> = { e in
+            return e.enumerated().flatMap { attempt, error -> Observable<Int> in
+                if attempt >= maxAttempts - 1 {
+                    return Observable.error(error)
+                } else if let casted = error as? ApiController.ApiError, casted == .invalidKey {
+                    return ApiController.shared.apiKey
+                        .filter { !$0.isEmpty }
+                        .map { _ in 1 }
+                }
+                else if (error as NSError).code == -1009 {
+                    return RxReachability.shared.status
+                        .filter { $0 == .online }
+                        .map { _ in 1 }
+                }
+                print("== retrying after \(attempt + 1) seconds ==")
+                return Observable<Int>.timer(.seconds(attempt + 1),
+                                             scheduler: MainScheduler.instance)
+                    .take(1)
+            }
+        }
+
+        let searchInput = searchCityName.rx.controlEvent(.editingDidEndOnExit)
+            .map { [weak self] _ in self?.searchCityName.text ?? "" }
+            .filter { !$0.isEmpty }
+
+        let textSearch = searchInput.flatMap { text in
+            return ApiController.shared.currentWeather(city: text)
+                .do(
+                    onNext: { [weak self] data in
+                        self?.cache[text] = data
+                    },
+                    onError: { error in
+                        DispatchQueue.main.async { [weak self] in
+                            guard let self = self else { return }
+                            self.showError(error: error)
+                        }
+                    }
+                )
+                .retryWhen(retryHandler)
+//                .retryWhen { e in
+//                    return e.enumerated().flatMap { attempt, error -> Observable<Int> in
+//                        if attempt >= maxAttempts - 1 {
+//                            return Observable.error(error)
+//                        }
+//                        print("== retrying after \(attempt + 1) seconds ==")
+//                        return Observable<Int>.timer(.seconds(attempt + 1),
+//                                                     scheduler: MainScheduler.instance)
+//                            .take(1)
+//                    }
+//                }
+                .catchError { [weak self] error in
+                    return Observable.just(self?.cache[text] ?? .empty)
+                }
+        }
+
+        let search = Observable.merge(geoSearch, textSearch)
+            .asDriver(onErrorJustReturn: .empty)
+
+        let running = Observable.merge(searchInput.map { _ in true },
+                                       geoInput.map { _ in true },
+                                       search.map { _ in false }.asObservable())
+            .startWith(true)
+            .asDriver(onErrorJustReturn: false)
+
+        search.map { "\($0.temperature)° C" }
+            .drive(tempLabel.rx.text)
+            .disposed(by:bag)
+
+        search.map(\.icon)
+            .drive(iconLabel.rx.text)
+            .disposed(by:bag)
+
+        search.map { "\($0.humidity)%" }
+            .drive(humidityLabel.rx.text)
+            .disposed(by:bag)
+
+        search.map(\.cityName)
+            .drive(cityNameLabel.rx.text)
+            .disposed(by:bag)
+
+        running.skip(1).drive(activityIndicator.rx.isAnimating).disposed(by:bag)
+        running.drive(tempLabel.rx.isHidden).disposed(by:bag)
+        running.drive(iconLabel.rx.isHidden).disposed(by:bag)
+        running.drive(humidityLabel.rx.isHidden).disposed(by:bag)
+        running.drive(cityNameLabel.rx.isHidden).disposed(by:bag)
     }
 
-    let geoSearch = geoLocation.flatMap { location in
-      return ApiController.shared.currentWeather(at: location.coordinate)
-        .catchErrorJustReturn(.empty)
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+
+        Appearance.applyBottomLine(to: searchCityName)
     }
 
-    let searchInput = searchCityName.rx.controlEvent(.editingDidEndOnExit)
-      .map { [weak self] _ in self?.searchCityName.text ?? "" }
-      .filter { !$0.isEmpty }
-
-    let textSearch = searchInput.flatMap { text in
-      return ApiController.shared.currentWeather(city: text)
-        .catchErrorJustReturn(.empty)
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
     }
 
-    let search = Observable.merge(geoSearch, textSearch)
-      .asDriver(onErrorJustReturn: .empty)
+    func requestKey() {
+        func configurationTextField(textField: UITextField!) {
+            self.keyTextField = textField
+        }
 
-    let running = Observable.merge(searchInput.map { _ in true },
-                                   geoInput.map { _ in true },
-                                   search.map { _ in false }.asObservable())
-      .startWith(true)
-      .asDriver(onErrorJustReturn: false)
+        let alert = UIAlertController(title: "Api Key",
+                                      message: "Add the api key:",
+                                      preferredStyle: UIAlertController.Style.alert)
 
-    search.map { "\($0.temperature)° C" }
-      .drive(tempLabel.rx.text)
-      .disposed(by:bag)
+        alert.addTextField(configurationHandler: configurationTextField)
 
-    search.map(\.icon)
-      .drive(iconLabel.rx.text)
-      .disposed(by:bag)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default) { [weak self] _ in
+            ApiController.shared.apiKey.onNext(self?.keyTextField?.text ?? "")
+        })
 
-    search.map { "\($0.humidity)%" }
-      .drive(humidityLabel.rx.text)
-      .disposed(by:bag)
+        alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertAction.Style.destructive))
 
-    search.map(\.cityName)
-      .drive(cityNameLabel.rx.text)
-      .disposed(by:bag)
-
-    running.skip(1).drive(activityIndicator.rx.isAnimating).disposed(by:bag)
-    running.drive(tempLabel.rx.isHidden).disposed(by:bag)
-    running.drive(iconLabel.rx.isHidden).disposed(by:bag)
-    running.drive(humidityLabel.rx.isHidden).disposed(by:bag)
-    running.drive(cityNameLabel.rx.isHidden).disposed(by:bag)
-  }
-
-  override func viewDidLayoutSubviews() {
-    super.viewDidLayoutSubviews()
-
-    Appearance.applyBottomLine(to: searchCityName)
-  }
-
-  override var preferredStatusBarStyle: UIStatusBarStyle {
-    return .lightContent
-  }
-
-  func requestKey() {
-    func configurationTextField(textField: UITextField!) {
-      self.keyTextField = textField
+        self.present(alert, animated: true)
     }
 
-    let alert = UIAlertController(title: "Api Key",
-                                  message: "Add the api key:",
-                                  preferredStyle: UIAlertController.Style.alert)
+    private func showError(error e: Error) {
+        guard let e = e as? ApiController.ApiError else {
+            InfoView.showIn(viewController: self, message: "An error occurred")
+            return
+        }
 
-    alert.addTextField(configurationHandler: configurationTextField)
+        switch e {
+        case .cityNotFound:
+            InfoView.showIn(viewController: self, message: "City Name is invalid")
+        case .serverFailure:
+            InfoView.showIn(viewController: self, message: "Server error")
+        case .invalidKey:
+            InfoView.showIn(viewController: self, message: "Key is invalid")
+        }
+    }
 
-    alert.addAction(UIAlertAction(title: "Ok", style: .default) { [weak self] _ in
-      ApiController.shared.apiKey.onNext(self?.keyTextField?.text ?? "")
-    })
+    // MARK: - Style
 
-    alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertAction.Style.destructive))
-
-    self.present(alert, animated: true)
-  }
-
-  // MARK: - Style
-
-  private func style() {
-    view.backgroundColor = UIColor.aztec
-    searchCityName.textColor = UIColor.ufoGreen
-    tempLabel.textColor = UIColor.cream
-    humidityLabel.textColor = UIColor.cream
-    iconLabel.textColor = UIColor.cream
-    cityNameLabel.textColor = UIColor.cream
-  }
+    private func style() {
+        view.backgroundColor = UIColor.aztec
+        searchCityName.textColor = UIColor.ufoGreen
+        tempLabel.textColor = UIColor.cream
+        humidityLabel.textColor = UIColor.cream
+        iconLabel.textColor = UIColor.cream
+        cityNameLabel.textColor = UIColor.cream
+    }
 }
